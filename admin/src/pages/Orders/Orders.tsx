854b706687e14d59e5d26ddd92c6de963aedd59f
Devansh032 <devanshsinghal627@gmail.com>
2025-03-12 18:51:15 +0530

    admin page completed

---
+++ admin/src/pages/Orders/Orders.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Orders.css'
+
+const Orders : React.FC< {url:string}> = ({url}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Orders
